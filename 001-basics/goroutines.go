e61986b21822c601a169173990de2054e0f15f1c
dwarakanathc24 <dwarakanathc24@gmail.com>
2024-04-02 15:53:16 +0100

    go routine

---
+++ 001-basics/goroutines.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+
+	var wg sync.WaitGroup
+	wg.Add(3)
+	go func() {
+		fmt.Println(" go routines 1")
+		wg.Done()
+	}()
+
+	go func() {
+		fmt.Println(" go routines 2")
+		wg.Done()
+	}()
+
+	go func() {
+		fmt.Println(" go routines 3")
+		wg.Done()
+	}()
+
+	wg.Wait()
+
+}
